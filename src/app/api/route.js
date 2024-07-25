bf0651f91a7c0b5ad2a1d21087fa4a63a22db267
noppavit1412 <66309010003@cmtc.ac.th>
2024-07-25 14:50:50 +0700

    test

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,17 +1,25 @@
+// app/api/route.js
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+client.connect();
 export async function GET() {
-    try {
-
-        return new Response(JSON.stringify({ message: "GET DATA OK อปโปะหม่ำๆ"}), {
+  try {
+        const result = await client.query('SELECT * FROM tbl_user');
+        return new Response(JSON.stringify(result.rows), {
             status: 200,
             headers: { "Content-Type": "application/json" },
-          });
-    } catch (error) {
+        });
+  } catch (error) {
+    
         return new Response(JSON.stringify({ error: "Internal Server Error" }), {
             status: 500,
             headers: { "Content-Type": "application/json" },
-          });
-
-    }
+        });
+  }
 }
 
 export async function POST() {
