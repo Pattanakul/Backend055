41d4ddf5efedf11eba87108b4d56e0241e7c863d
noppavit1412 <66309010003@cmtc.ac.th>
2024-07-25 13:42:04 +0700

    first commit

---
+++ src/app/api/route.js
@@ -0,0 +1,63 @@
+export async function GET() {
+    try {
+
+        return new Response(JSON.stringify({ message: "GET DATA OK อปโปะหม่ำๆ"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    }
+}
+
+export async function POST() {
+    try {
+
+        return new Response(JSON.stringify({ message: "POST DATA OK อปโปะหม่ำๆ"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    }
+}
+
+export async function PUT() {
+    try {
+
+        return new Response(JSON.stringify({ message: "PUT DATA OK อปโปะหม่ำๆ"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    }
+}
+
+export async function DELETE() {
+    try {
+
+        return new Response(JSON.stringify({ message: "DELETE DATA OK อปโปะหม่ำๆ"}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          });
+
+    }
+}
